987d28ccb2139962eb31ab2033503ca3ec17954b
Ed <edithvelez.unam.1411@gmail.com>
2025-11-07 10:48:09 -0600

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module fes.unam.aragon.metodosorden {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens fes.unam.aragon.metodosorden to javafx.fxml;
+    exports fes.unam.aragon.metodosorden;
+}
