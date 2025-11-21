0eeb7057a452571cdbd6e147ecc13b99982431e0
samzhand <edgar18tor@gmail.com>
2025-11-20 20:39:15 -0600

    Se-agrega-funcionalidad-de-btnNuevosdatos-solo-es-grafico-aun-no-implementa-estructura-de-datos

--- src/main/java/fes/unam/aragon/metodosorden/controlador/Controlador.java
+++ src/main/java/fes/unam/aragon/metodosorden/controlador/Controlador.java
@@ -18,6 +18,8 @@ import java.util.ResourceBundle;
 
 public class Controlador implements Initializable {
     ListaSimple<Integer> listaSimple=new ListaSimple<>();
+    int tiempoRetardo= 40;
+    int numeroDatos=40;
 
     @FXML
     private Button btnLista;
@@ -38,15 +40,22 @@ public class Controlador implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         bxSelector.getItems().addAll(opciones);
         bxSelector.setValue("Burbuja");
-        aleatorios();
-        mostrarDatosEnGrafica();
+        chGrafica.setAnimated(false);
+        XYChart.Series<String,Number> series=new XYChart.Series<String,Number>();
+        series= generarAleatorios(numeroDatos);
+        chGrafica.getData().add(series);
+
     }
 
 
     @FXML
     void btnLista(ActionEvent event) {
-        aleatorios();
-        mostrarDatosEnGrafica();
+        //System.out.println(generarAleatorios(40));
+        chGrafica.getData().clear();
+        XYChart.Series<String,Number> series=new XYChart.Series<String,Number>();
+        series=generarAleatorios(numeroDatos);
+        chGrafica.getData().add(series);
+
 
     }
 
@@ -54,38 +63,36 @@ public class Controlador implements Initializable {
     void btnOrdenar(ActionEvent event) {
         String selecciono = bxSelector.getValue();
         ordenarDatos(selecciono);
-        mostrarDatosEnGrafica();
 
     }
 
 
-    private void mostrarDatosEnGrafica(){
-        chGrafica.getData().clear();
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
-
-        for (int i = 0; i < listaSimple.getLongitud(); i++) {
-            String nombre = ""+listaSimple.obtenerNodo(i);
-            Number valor = listaSimple.obtenerNodo(i);
-            series.getData().add(new XYChart.Data<>(nombre, valor));
-        }
-        chGrafica.getData().add(series);
-    }
-    private void aleatorios(){
-        Random random = new Random();
-        listaSimple.vaciarLista();
-        for (int i = 0; i < 40; i++) {
-            Integer num = random.nextInt(100);
-            listaSimple.agregarEnCola(num);
-        }
-        listaSimple.imprimirLista();
-    }
+//    private void mostrarDatosEnGrafica(){
+//        chGrafica.getData().clear();
+//        XYChart.Series<String, Number> series = new XYChart.Series<>();
+//
+//        for (int i = 0; i < listaSimple.getLongitud(); i++) {
+//            String nombre = ""+listaSimple.obtenerNodo(i);
+//            Number valor = listaSimple.obtenerNodo(i);
+//            series.getData().add(new XYChart.Data<>(nombre, valor));
+//        }
+//        chGrafica.getData().add(series);
+//    }
+//    private void aleatorios(){
+//        Random random = new Random();
+//        listaSimple.vaciarLista();
+//        for (int i = 0; i < numeroDatos; i++) {
+//            Integer num = random.nextInt(100);
+//            listaSimple.agregarEnCola(num);
+//        }
+//        listaSimple.imprimirLista();
+//    }
     private void ordenarDatos(String metodo){
         switch (metodo){
             case "Burbuja":
                 Burbuja<Integer> burbuja=new Burbuja(listaSimple);
                 burbuja.ordenarAscendente();
                 listaSimple=burbuja.getLista();
-                mostrarDatosEnGrafica();
                 listaSimple.imprimirLista();
                 break;
             case "Insercion":
@@ -105,5 +112,15 @@ public class Controlador implements Initializable {
         alerta.setHeaderText("Error");
         alerta.setContentText(mensaje+"\nRevise bien los datos ingresados");
         alerta.showAndWait();
+    }
+
+    private XYChart.Series<String, Number>generarAleatorios(int n){
+        XYChart.Series<String, Number> series=new XYChart.Series<>();
+        Random rnd=new Random();
+        for (int i = 1; i <n ; i++) {
+            int al=rnd.nextInt(50);
+            series.getData().add(new XYChart.Data<>(String.valueOf(al),al));
+        }
+        return series;
     }
 }
